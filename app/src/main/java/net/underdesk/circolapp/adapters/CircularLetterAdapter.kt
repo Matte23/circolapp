27001c9ea46aaaf7a8ff134b4bac77d3177d30ba
Matte23 <matteo@underdesk.net>
2019-09-16 20:22:49 +0200

    Parse date and title from circular string

--- app/src/main/java/net/underdesk/circolapp/adapters/CircularLetterAdapter.kt
+++ app/src/main/java/net/underdesk/circolapp/adapters/CircularLetterAdapter.kt
@@ -18,6 +18,7 @@
 
 package net.underdesk.circolapp.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -30,22 +31,26 @@ import net.underdesk.circolapp.data.Circular
 
 class CircularLetterAdapter(private val circulars: List<Circular>) :
     RecyclerView.Adapter<CircularLetterAdapter.CircularLetterViewHolder>() {
+    private lateinit var context: Context
 
     inner class CircularLetterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var title: TextView = view.circular_title_textview
         var number: TextView = view.circular_number_textview
+        var date: TextView = view.circular_date_textview
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CircularLetterViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_circular, parent, false)
+        context = parent.context
 
         return CircularLetterViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: CircularLetterViewHolder, position: Int) {
-        holder.number.text = circulars[position].id.toString()
+        holder.number.text = context.getString(R.string.notification_title, circulars[position].id)
         holder.title.text = circulars[position].name
+        holder.date.text = circulars[position].date
     }
 
     override fun getItemCount() = circulars.size
